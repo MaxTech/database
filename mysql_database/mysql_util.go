ca406b522cee7f913f2405dd00698d4271be6baa
Maximilian Wong <maximilian520@163.com>
2019-03-30 12:32:25 +0800

    update util

--- mysql_database/mysql_util.go
+++ mysql_database/mysql_util.go
@@ -7,27 +7,32 @@ import (
     "log"
 )
 
+type util struct {
+}
+
+var Util *util
+
+func init() {
+    Util = new(util)
+}
+
 type mySQLPool struct {
     linkString string
     loggerName string
-    logSqlFlag bool
+    logSql bool
 }
 
-var MySQLPool *mySQLPool
-
-func init() {
-    MySQLPool = new(mySQLPool)
-}
-
-func (mp *mySQLPool) Init(address, username, password, dbname string, logSql bool) {
-    mp.linkString = fmt.Sprintf(
-        "%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&timeout=2s",
-        username,
-        password,
-        address,
-        dbname)
-    mp.loggerName = dbname
-    mp.logSqlFlag = logSql
+func (u *util) Init(address, username, password, dbname string, logSql bool) *mySQLPool {
+    return &mySQLPool{
+        linkString: fmt.Sprintf(
+            "%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&timeout=2s",
+            username,
+            password,
+            address,
+            dbname),
+            loggerName: dbname,
+            logSql: logSql,
+    }
 }
 
 func (mp *mySQLPool) GetEngine() *xorm.Engine {
@@ -35,8 +40,8 @@ func (mp *mySQLPool) GetEngine() *xorm.Engine {
     if err != nil {
         log.Fatal("Database connect error: ", err)
     }
-    engine.ShowExecTime(mp.logSqlFlag)
-    engine.ShowSQL(mp.logSqlFlag)
+    engine.ShowExecTime(mp.logSql)
+    engine.ShowSQL(mp.logSql)
     engine.SetLogger(
         NewSqlLogger(fmt.Sprintf("[%s]", mp.loggerName), xorm.DEFAULT_LOG_FLAG))
     return engine
