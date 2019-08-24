4925874a8065639c610207f8eee4913effff563f
Maximilian <maximilian520@163.com>
2019-08-25 00:38:21 +0800

    update

--- mysql_database/config_format.go
+++ mysql_database/config_format.go
@@ -1,8 +1,8 @@
 package mysql_database
 
 type MySQLConfigFormat struct {
-    Address  string `yaml:"address"`
-    Username string `yaml:"username"`
-    Password string `yaml:"password"`
-    DBName   string `yaml:"db_name"`
+    Address  string `json:"address" yaml:"address"`
+    Username string `json:"username" yaml:"username"`
+    Password string `json:"password" yaml:"password"`
+    DBName   string `json:"db_name" yaml:"db_name"`
 }
